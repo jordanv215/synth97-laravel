b82020f484f90bbff41a932c3b035b163f893398
Jordan Vinson <jordanv215@gmail.com>
2020-09-30 17:28:05 -0600

    ADDED comments

--- synth97-ppwa/routes/web.php
+++ synth97-ppwa/routes/web.php
@@ -29,18 +29,21 @@ Route::get('/playground', function () {
 });
 
 // wildcare route
-
 Route::get('/posts/{post}', function ($post) {
 
+    // associative array to simulate a db of posts
     $posts = [
         'my-first-post' => 'Hello, this is the first post',
         'my-second-post' => 'Yo, this is the second post'
     ];
 
+
+    // if the post doesnt exist in the datastore above, show 404
     if (! array_key_exists($post, $posts)) {
         abort(404, 'Sorry, this post was not found.');
     }
 
+    // if it does exist, pull up the post
     return view('post', [
         'post' => $posts[$post]
     ]);

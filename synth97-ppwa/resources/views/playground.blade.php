85674aeb49690e79373a2cffb80d59e0ff3c6ede
Jordan Vinson <jordanv215@gmail.com>
2020-09-30 17:14:55 -0600

    ADDED an escape to avoid user entry

--- synth97-ppwa/resources/views/playground.blade.php
+++ synth97-ppwa/resources/views/playground.blade.php
@@ -6,7 +6,8 @@
     <title>Synth 97</title>
   </head>
   <body>
-    <h1><?= $name; ?></h1>
+    <!-- this will escape quotes so the user cant trigger a script from the url like http://127.0.0.1:8000/playground/?name=%3Cscript%3Ealert(%27?%27);%3C/script%3E -->
+    <h1><?= htmlspecialchars($name, ENT_QUOTES)?></h1>
   </body>
 </html>
 

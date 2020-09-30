16143bb04021b14a7d86bebbeafd8b6f151f559e
Jordan Vinson <jordanv215@gmail.com>
2020-09-30 17:12:20 -0600

    ADDED html shell

--- synth97-ppwa/resources/views/playground.blade.php
+++ synth97-ppwa/resources/views/playground.blade.php
@@ -1,1 +1,12 @@
-<h1><?= $name; ?></h1>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Synth 97</title>
+  </head>
+  <body>
+    <h1><?= $name; ?></h1>
+  </body>
+</html>
+

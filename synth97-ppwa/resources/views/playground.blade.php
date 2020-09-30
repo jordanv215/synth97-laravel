05737ae240827236eee6e1d1365b9b0ac1964277
Jordan Vinson <jordanv215@gmail.com>
2020-09-30 17:15:39 -0600

    same as last commit but with moustache syntax (the laravel way)

--- synth97-ppwa/resources/views/playground.blade.php
+++ synth97-ppwa/resources/views/playground.blade.php
@@ -7,7 +7,7 @@
   </head>
   <body>
     <!-- this will escape quotes so the user cant trigger a script from the url like http://127.0.0.1:8000/playground/?name=%3Cscript%3Ealert(%27?%27);%3C/script%3E -->
-    <h1><?= htmlspecialchars($name, ENT_QUOTES)?></h1>
+    <h1>{{ $name }}</h1>
   </body>
 </html>
 

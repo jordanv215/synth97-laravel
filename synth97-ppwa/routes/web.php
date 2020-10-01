4e5b63359a66180fb0fabfa1c73c02a235871513
Jordan Vinson <jordanv215@gmail.com>
2020-09-30 20:38:43 -0600

    ADDED controller route and ADDED a posts controller with a show method

--- synth97-ppwa/routes/web.php
+++ synth97-ppwa/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
+// controller route
+Route::get('/posts/{post}', [PostsController::class, 'show']);
 
 Route::get('/', function () {
     return view('landing');
@@ -29,23 +34,24 @@ Route::get('/playground', function () {
 });
 
 // wildcare route
-Route::get('/posts/{post}', function ($post) {
+// Route::get('/posts/{post}', function ($post) {
 
-    // associative array to simulate a db of posts
-    $posts = [
-        'my-first-post' => 'Hello, this is the first post',
-        'my-second-post' => 'Yo, this is the second post'
-    ];
+//     // associative array to simulate a db of posts
+//     $posts = [
+//         'my-first-post' => 'Hello, this is the first post',
+//         'my-second-post' => 'Yo, this is the second post'
+//     ];
 
 
-    // if the post doesnt exist in the datastore above, show 404
-    if (! array_key_exists($post, $posts)) {
-        abort(404, 'Sorry, this post was not found.');
-    }
+//     // if the post doesnt exist in the datastore above, show 404
+//     if (! array_key_exists($post, $posts)) {
+//         abort(404, 'Sorry, this post was not found.');
+//     }
 
-    // if it does exist, pull up the post
-    return view('post', [
-        'post' => $posts[$post]
-    ]);
+//     // if it does exist, pull up the post
+//     return view('post', [
+//         'post' => $posts[$post]
+//     ]);
 
-});
+// });
+

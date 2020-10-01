a84a77b99c247de93181c512d38631d3c3ed08ad
Jordan Vinson <jordanv215@gmail.com>
2020-09-30 20:41:53 -0600

    ADDED previous route content with associative array to the controller show method instead

--- synth97-ppwa/app/Http/Controllers/PostsController.php
+++ synth97-ppwa/app/Http/Controllers/PostsController.php
@@ -4,7 +4,21 @@ namespace App\Http\Controllers;
 
 class PostsController
 {
-    public function show() {
-        return 'hello';
+    public function show($post) {
+        $posts = [
+            'my-first-post' => 'Hello, this is the first post',
+            'my-second-post' => 'Yo, this is the second post'
+        ];
+
+
+        // if the post doesnt exist in the datastore above, show 404
+        if (! array_key_exists($post, $posts)) {
+            abort(404, 'Sorry, this post was not found.');
+        }
+
+        // if it does exist, pull up the post
+        return view('post', [
+            'post' => $posts[$post]
+        ]);
     }
 }

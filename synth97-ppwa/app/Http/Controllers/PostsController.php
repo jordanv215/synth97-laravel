12758d84aa94a1dd1386cbd23dce294a2c776dd9
Jordan Vinson <jordanv215@gmail.com>
2020-10-01 17:33:32 -0600

    changed first to firstOrFail so I can remove the 404 check

--- synth97-ppwa/app/Http/Controllers/PostsController.php
+++ synth97-ppwa/app/Http/Controllers/PostsController.php
@@ -9,15 +9,7 @@ class PostsController extends Controller
 {
     public function show($slug) {
 
-        // $post = \DB::table('posts')->where('slug', $slug)->first();
-        $post = Post::where('slug', $slug)->first();
-
-        if (! $post) {
-            abort(404);
-        }
-
-        // dd($post);
-
+        $post = Post::where('slug', $slug)->firstOrFail();
 
         // if it does exist, pull up the post
         return view('post', [

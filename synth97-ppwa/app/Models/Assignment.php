7a4266ec35d60dd056ef9a4500851efed89b99cb
Jordan Vinson <jordanv215@gmail.com>
2020-10-01 18:11:20 -0600

    ADDED complete method to assignment model

--- synth97-ppwa/app/Models/Assignment.php
+++ synth97-ppwa/app/Models/Assignment.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Assignment extends Model
 {
     use HasFactory;
+
+    public function complete(){
+        $this->completed = true;
+
+        $this->save();
+    }
 }

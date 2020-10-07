ae20fb4b79f4bdf0509e5332bd58b418c5a8cbe2
Jordan Vinson <jordanv215@gmail.com>
2020-10-06 21:58:40 -0600

    ADDED footer to the layout file

--- synth97-ppwa/resources/views/example.blade.php
+++ synth97-ppwa/resources/views/example.blade.php
@@ -13,7 +13,5 @@
 @endsection
 
 @section ('content')
-    <div id="copyright" class="container">
-        <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
-    </div>
+
 @endsection

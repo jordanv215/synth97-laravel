02cb079ee941f985187e4c9a2dd7decd6177463a
Jordan Vinson <jordanv215@gmail.com>
2020-10-06 21:06:49 -0600

    READDED header and nav to about to be clenaed

--- synth97-ppwa/resources/views/about.blade.php
+++ synth97-ppwa/resources/views/about.blade.php
@@ -1,17 +1,6 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title></title>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
-<link href="css/default.css" rel="stylesheet" />
-<link href="css/fonts.css" rel="stylesheet" />
-</head>
-<body>
-    @yield ('content')
-</body>
-</html>
+@extends ('layout')
+
+@section ('content')
   <div id="wrapper">
         <div id="page" class="container">
             <div id="content">
@@ -64,3 +53,4 @@
     <div id="copyright" class="container">
         <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
     </div>
+@endsection

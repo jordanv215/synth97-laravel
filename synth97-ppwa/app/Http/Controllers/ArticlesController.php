17e7871b3f8463d281ae5d2ad20787f133890493
Jordan Vinson <jordanv215@gmail.com>
2020-10-07 20:58:44 -0600

    ADDED some articles controller changes and created a new view for the single article.

--- synth97-ppwa/app/Http/Controllers/ArticlesController.php
+++ synth97-ppwa/app/Http/Controllers/ArticlesController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class ArticlesController extends Controller
 {
-    public function show($articleId) {
-        dd($articleId);
+    public function show($id) {
+        $article = Article::find($id);
+
+        return view('articles.show', ['article' => $article]);
     }
 }
